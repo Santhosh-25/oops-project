03b1bcc054787a34ff38a0a7ead18ac1d4c9e871
santhosh <santhoshmahii25@gmail.com>
2023-07-06 11:30:16 +0530

    modules file added

---
+++ modules.js
@@ -0,0 +1,20 @@
+export default class User {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+}
+
+export function printName(user) {
+  console.log(`User name is ${user.name}`);
+}
+
+export function printAge(user) {
+  console.log(`User is ${user.age} years old`);
+}
+
+//default export
+//export default User;
+
+//standard export
+//export { printName, printAge };
